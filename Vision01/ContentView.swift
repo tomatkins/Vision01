a82e67b44e1d206681ae0cb3f06a0bddffb7acb9
Tom Atkins <treat@mac.com>
2024-09-23 23:28:29 -0400

    Initial Commit

---
+++ Vision01/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Vision01
+//
+//  Created by Tom Atkins on 9/23/24.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ Vision01/Vision01App.swift
@@ -0,0 +1,17 @@
+//
+//  Vision01App.swift
+//  Vision01
+//
+//  Created by Tom Atkins on 9/23/24.
+//
+
+import SwiftUI
+
+@main
+struct Vision01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
